c6efe214b67659b4c3a3165a249620fa0af714a9
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-02 15:27:59 +0530

    fixed the error which showed for lowerCasePage

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -8,7 +8,7 @@ type Props = {
 };
 
 const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
-  const lowerCasePage = page.toLowerCase().replace(/ /g, "");
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
   return (
     <AnchorLink
       className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}

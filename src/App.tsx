f0e802fe3bf95920e989d90a19aaca8562e95d40
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-02 14:28:42 +0530

    navbar menu implemented from Link to index to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 
 import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+useState
 function App() {
+
+  const [selectedPage, setSelectedPage] = useState("home");
 
   return (
       <div className="app bg-gray-20">
-        <Navbar />
+        <Navbar 
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+
+        />
       </div>
     
   )
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
+import Link from "./Link";
 
-type Props = {};
+type Props = {
+    selectedPage: string;
+    setSelectedPage: (value: string) => void;
+};
 
-const Navbar = (props: Props) => {
+const Navbar = ({selectedPage, setSelectedPage}: Props) => {
   const flexBetween = "flex items-center justify-between";
   return <nav>
     <div className={`${flexBetween} fixed top-0 z-30 w-pul py-6`}>
@@ -18,10 +22,29 @@ const Navbar = (props: Props) => {
 
 {/* inner left side like home   */}
                 <div className={`${flexBetween} gap-8 text-sm`}>  
-                <p>Home</p>
-                <p>Benefits</p>
-                <p>Our classes</p>
-                <p>Contact us</p>
+                <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+
+                <Link 
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                />
+
+                <Link 
+                    page="Our classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                />
+
+                <Link 
+                    page="Contact us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                />
 
                 </div>
 

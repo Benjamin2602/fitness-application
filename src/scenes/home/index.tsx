75ed50b088a42b8bff51b2d961187d71aaa9c945
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-03 13:37:56 +0530

    home header with join now button

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -6,17 +6,52 @@ import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
-    setSelectedPage: (value: SelectedPage) => void;
-}
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Home = ({setSelectedPage}: Props) => {
-    const isAboveMediumScreens = useMediaQuery("(max-width: 1060px)")
+const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(max-width: 1060px)");
   return (
-    <section 
-    id="home"
-    className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"> Home</section>
-  )
-}
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* Image and Main Header */}
+      <div>
+        {/* main header */}
+        <div>
+          {/* headings */}
+          <div>
+            <div>
+              <div>
+                <img src={HomePageText} alt="text" />
+              </div>
+            </div>
+            <p>
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>
+          </div>
+
+          {/* Actions */}
+          <div>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>learn more</p>
+            </AnchorLink>
+          </div>
+        </div>
+        {/* image */}
+        <div></div>
+      </div>
+    </section>
+  );
+};
 export default Home;

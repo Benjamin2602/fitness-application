0222026f327804bcab1248434c1a9a5aafdf7ce2
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-03 15:35:52 +0530

    styling done for Sponsors

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -61,9 +61,9 @@ const Home = ({ setSelectedPage }: Props) => {
 
       {/* sponsors */}
       {isAboveMediumScreens && (
-        <div>
-          <div>
-            <div>
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
               <img alt="redbull-sponsor" src={SponsorRedBull} />
               <img alt="forbes-sponsor" src={SponsorForbes} />
               <img alt="fortune-sponsor" src={SponsorFortune} />

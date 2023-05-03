0305d9180ee1711e70d0b7b67f4b7ec501753ad4
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-03 15:31:04 +0530

    styling and positioning for Image and Main Header

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -17,17 +17,19 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* Image and Main Header */}
-      <div>
+      <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
         {/* main header */}
-        <div>
+        <div className="z-10 mt-32 md:basis-3/5">
           {/* headings */}
-          <div>
-            <div>
-              <div>
+          <div className="md:-mt-20">
+            {/* parent div tag */}
+            <div className="relative">
+              {/* evolve text {child div tag} */}
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] md:before:content-evolvetext">
                 <img src={HomePageText} alt="text" />
               </div>
             </div>
-            <p>
+            <p className="mt-8 text-sm">
               Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
               Studios to get the Body Shapes That you Dream of.. Get Your Dream
               Body Now.
@@ -35,7 +37,7 @@ const Home = ({ setSelectedPage }: Props) => {
           </div>
 
           {/* Actions */}
-          <div>
+          <div className="mt-8 flex items-center gap-8 ">
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
@@ -49,7 +51,10 @@ const Home = ({ setSelectedPage }: Props) => {
           </div>
         </div>
         {/* image */}
-        <div>
+        <div
+          className="flex basis-3/5 justify-center md:z-10
+                md:ml-40 md:mt-16 md:justify-items-end"
+        >
           <img alt="home-pageGraphic" src={HomePageGraphic} />
         </div>
       </div>

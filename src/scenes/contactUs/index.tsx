1403c86d4a25e32cf5e47dbb9bd37bc08753dceb
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-09 16:20:10 +0530

    Created ContactUs component!

---
+++ src/scenes/contactUs/index.tsx
@@ -0,0 +1,13 @@
+
+import { SelectedPage } from "@/shared/types";
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default ContactUs

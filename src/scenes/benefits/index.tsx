0d2fc7f8e590cf21ed1740d09dc27ce7d5d554e1
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-09 11:39:18 +0530

    Image and description section done in benefits pge

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -8,6 +8,8 @@ import { BenefitType, SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 import Htext from "@/shared/Htext";
 import Benefit from "./Benefit";
+import ActionButton from "@/shared/ActionButton";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
 
 //benefits=>  array of objects that contains the data for the benefits
 //BenefitType=> type of the benefit object
@@ -57,7 +59,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
-          transition={{  duration: 0.5 }}
+          transition={{ duration: 0.5 }}
           variants={{
             hidden: { opacity: 0, x: -50 },
             visible: { opacity: 1, x: 0 },
@@ -91,6 +93,61 @@ const Benefits = ({ setSelectedPage }: Props) => {
             />
           ))}
         </motion.div>
+
+        {/* GRAPHICS AND DESCRIPTION */}
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          {/* Graphic */}
+          <img
+            className="mx-auto"
+            alt="benefits-page-graphics"
+            src={BenefitsPageGraphic}
+          />
+
+          {/* Description */}
+          <div>
+            {/* title */}
+            <div className="relative">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] before:content-abstractwaves">
+                <div>
+                  <Htext>
+                    MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                    <span className="text-primary-500">FIT</span>
+                  </Htext>
+                </div>
+              </div>
+            </div>
+
+            {/* description */}
+            <div>
+              <p className="my-5">
+                Millions of happy members getting represents a thriving
+                community of individuals who have successfully achieved their
+                fitness goals and are reaping the benefits of a healthy
+                lifestyle. By making fitness a priority in their lives, these
+                individuals have transformed their physical and mental
+                wellbeing, experiencing improved energy levels, increased
+                strength and endurance, and enhanced mood and mental clarity.
+              </p>
+              <p className="mb-5">
+                Being part of a community of millions of happy members getting
+                offers many benefits for individuals who are on their own
+                fitness journey.By joining this community, individuals gain
+                access to a wealth of knowledge and resources, including advice
+                and support from experts and fellow members who have
+                successfully achieved their fitness goals.
+              </p>
+            </div>
+
+            {/* button */}
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </div>
+            </div>
+          </div>
+        </div>
       </motion.div>
     </section>
   );

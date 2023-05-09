48955c00512cef1747d16ceb9c0695cea905febd
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-09 16:35:23 +0530

    Header Section done for ContactUs page

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,13 +1,43 @@
-
 import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import Htext from "@/shared/Htext";
+
 type Props = {
-    setSelectedPage: (value: SelectedPage) => void;
-}
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
 const ContactUs = ({ setSelectedPage }: Props) => {
   return (
-    <div>index</div>
-  )
-}
+    <section id="contactUs" className="mx-auto w-5/6 pb-32 pt-24">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+      >
+        {/* Header */}
+        <motion.div
+          className="md: w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <Htext>
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+          </Htext>
+          <p className="my-5">
+            Our program is designed to provide individuals with a comprehensive
+            and personalized workout experience that is tailored to their unique
+            fitness needs and goals.
+          </p>
+        </motion.div>
+      </motion.div>
+    </section>
+  );
+};
 
-export default ContactUs
+export default ContactUs;

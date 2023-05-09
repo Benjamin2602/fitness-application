8f56d959c3f99eadde5d4eced4184758d3a263ba
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-09 12:37:12 +0530

    OurClasses header section done

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,15 +1,47 @@
-import { SelectedPage, ClassType } from "@/shared/types";
-
-
+import { SelectedPage } from "@/shared/types";
+import image1 from "@/assets/image1.png";
+import image2 from "@/assets/image2.png";
+import image3 from "@/assets/image3.png";
+import image4 from "@/assets/image4.png";
+import image5 from "@/assets/image5.png";
+import image6 from "@/assets/image6.png";
+import { motion } from "framer-motion";
+import Htext from "@/shared/Htext";
 
 type Props = {
-    setSelectedPage: (value: SelectedPage) => void;
-}
+  setSelectedPage: (value: SelectedPage) => void;
+};
 const OurClasses = ({ setSelectedPage }: Props) => {
   return (
-    <div>OurClasses</div>
-  )
-}
-
+    <section id="ourclasses" className="w-full bg-primary-100 py-40">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+      >
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="md:w-3/5">
+            <Htext>OUR CLASSES</Htext>
+            <p className="py-5">
+              Our classes are designed to provide individuals with a fun,
+              engaging, and effective workout experience. Whether you are a
+              beginner or an experienced fitness enthusiast, our classes are
+              tailored to meet your specific fitness needs and goals.
+            </p>
+          </div>
+        </motion.div>
+        
+      </motion.div>
+    </section>
+  );
+};
 
 export default OurClasses;

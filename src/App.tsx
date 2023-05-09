05e9bea2679b3871cdb7b5a1d2c8b5608dd67478
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-09 11:56:07 +0530

    Created OurClasses page component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
+import OurClasses from "@/scenes/ourClasses";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
 
@@ -34,6 +35,7 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage}/>
       <Benefits setSelectedPage={setSelectedPage}/>
+      <OurClasses setSelectedPage={setSelectedPage} />
       
       </div>
     
---
+++ src/scenes/ourClasses/index.tsx
@@ -0,0 +1,15 @@
+import { SelectedPage, ClassType } from "@/shared/types";
+
+
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+}
+const OurClasses = ({ setSelectedPage }: Props) => {
+  return (
+    <div>OurClasses</div>
+  )
+}
+
+
+export default OurClasses;

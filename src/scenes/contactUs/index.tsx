6350211180752268f158f09056b305b797e124f2
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-11 19:11:10 +0530

    image section done in contactus

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -142,6 +142,27 @@ const ContactUs = ({ setSelectedPage }: Props) => {
               </button>
             </form>
           </motion.div>
+
+          {/* image */}
+          <motion.div
+            className="relative mt-16 basis-2/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{delay:0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <div className="md:before:content-evolvetext w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] ">
+              <img
+              className="w-full"
+              alt="contact-us-page-graphic"
+              src={ContactUsPageGraphic}
+              />
+            </div>
+          </motion.div>
         </div>
       </motion.div>
     </section>

3485cf7e4252aac51dd7cf90db3d41908a4789a0
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-11 17:48:07 +0530

    form section is done! using FormSubmit

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -9,6 +9,23 @@ type Props = {
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
+  const inputStyles = `mb-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
+
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  //any keyword is used to avoid typescript error
+  const onSubmit = async (e: any) => {
+    //trigger basically allows us to validate the form if needed
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
+
   return (
     <section id="contactUs" className="mx-auto w-5/6 pb-32 pt-24">
       <motion.div
@@ -35,6 +52,97 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             fitness needs and goals.
           </p>
         </motion.div>
+        {/* Form and Image */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          {/* Form */}
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <form
+              target="_blank"
+              onSubmit={onSubmit}
+              action="https://formsubmit.co/benjaminsamuel2602@gmail.com"
+              method="POST"
+            >
+              {/* name */}
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="Name"
+                //basically react hook form registering your input by passing in the name of the input
+                {...register("name", {
+                  required: true,
+                  maxLength: 50,
+                })}
+              />
+              {/* error condition in react-hook-form */}
+              {errors.name && (
+                <p className="mb-3 text-red-500">
+                  {errors.name.type === "required" && "Name is required"}
+                  {errors.name.type === "maxLength" &&
+                    "max length cannot exceed 50 characters"}
+                </p>
+              )}
+
+              {/* email */}
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="EMAIL"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              />
+              {/* error condition */}
+              {errors.email && (
+                <p className="mb-3 text-primary-500">
+                  {errors.email.type === "required" &&
+                    "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid email address."}
+                </p>
+              )}
+
+              {/* message */}
+
+              <textarea
+                className={inputStyles}
+                placeholder="MESSAGE"
+                rows={4}
+                cols={50}
+                {...register("message", {
+                  required: true,
+                  maxLength: 2000,
+                })}
+              />
+              {errors.message && (
+                <p className="mb-1 text-primary-500">
+                  {errors.message.type === "required" &&
+                    "This field is required."}
+                  {errors.message.type === "maxLength" &&
+                    "Max length is 2000 char."}
+                </p>
+              )}
+
+              {/* submit button */}
+
+              <button
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+              >
+                SUBMIT
+              </button>
+            </form>
+          </motion.div>
+        </div>
       </motion.div>
     </section>
   );
